a4f0fe5dfa76d2aa93f2e1dd678538fbcb81d63c
caiotaavares <caio_s.tavares2@hotmail.com>
2021-09-27 20:11:47 -0300

    nodemon confing

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 
 app.listen(5000, () => {
-    console.log('rodando na porta 5000');
-})
+    console.log('Aplicação rodando na porta 5000');
+});
